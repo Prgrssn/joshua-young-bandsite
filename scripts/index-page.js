07de156580e2bfb11035094b8da29cc87a7160c1
Joshua Young <prgrssn@gmail.com>
2021-10-17 11:12:37 -0700

    added some additional styling to the comment section in mobile view. adjusted margins, padding and fonts. Added the avatar image for the comment form and now figuring out how to position it to resemble mockup.

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,22 +1,20 @@
 const commentArr = [
   {
     name: "Connor Walton",
-    date: "02 / 17 / 2021",
+    date: "02/17/2021",
     text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
   {
     name: "Emilie Beach",
-    date: "01 / 09 / 2021",
+    date: "01/09/2021",
     text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
     name: "Miles Acosta",
-    date: "12 / 20 / 2020",
+    date: "12/20/2020",
     text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
 ];
-
-console.log(commentArr);
 
 // Comments Section
 const commentContainer = document.querySelector(
@@ -71,7 +69,7 @@ const handleCommentSubmit = (event) => {
   let month = date.getMonth() + 1;
   let day = date.getDate();
   let year = date.getFullYear();
-  let newDate = `${month} / ${day} / ${year}`;
+  let newDate = `${month}/${day}/${year}`;
 
   const commentData = {
     name: event.target.name.value,
