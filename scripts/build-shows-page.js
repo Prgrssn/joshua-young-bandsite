3bd7879b6f0da3c9383f384e872b495b75591f5f
Joshua Young <prgrssn@gmail.com>
2021-10-19 17:31:22 -0700

    removed the excess comments and code left over after completing the shows table

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,38 +1,3 @@
-// Shows Array
-
-// const showsArray = [
-//   {
-//     date: "Mon Sept 06 2021",
-//     place: "Ronald Lane  ",
-//     location: "San Francisco, CA",
-//   },
-//   {
-//     date: "Tue Sept 21 2021",
-//     place: "Pier 3 East  ",
-//     location: "San Francisco, CA",
-//   },
-//   {
-//     date: "Fri Oct 15 2021 ",
-//     place: "View Lounge ",
-//     location: "San Francisco, CA",
-//   },
-//   {
-//     date: "Sat Nov 06 2021 ",
-//     place: "Hyatt Agency ",
-//     location: "San Francisco, CA",
-//   },
-//   {
-//     date: "Fri Nov 26 2021 ",
-//     place: "Moscow Center",
-//     location: "San Francisco, CA",
-//   },
-//   {
-//     date: "Wed Dec 15 2021 ",
-//     place: "Press Club   ",
-//     location: "San Francisco, CA",
-//   },
-// ];
-
 // shows API:
 const showsAPI =
   "https://project-1-api.herokuapp.com/showdates?api_key=0f383f02-d3bd-4c8d-9bee-b53825c23122";
@@ -50,50 +15,6 @@ const displayShowsSectionHeader = () => {
 };
 
 displayShowsSectionHeader();
-
-// get shows:
-
-// const getComments = (result) => {
-//   axios
-//     .get(showsAPI)
-//     .then((result) => {
-//       console.log(result);
-
-//       const commentContent = (element) => {
-//         element.forEach(() => {
-//           const tableRow = document.createElement("tr");
-//           tableRow.classList.add("show-table__show-row");
-
-//           const showDate = document.createElement("td");
-//           showDate.classList.add("show-table__text--date");
-//           showDate.innerText = element.date;
-
-//           const showVenue = document.createElement("td");
-//           showVenue.classList.add("show-table__text");
-//           showVenue.innerText = element.place;
-
-//           const showLocation = document.createElement("td");
-//           showLocation.classList.add("show-table__text");
-//           showLocation.innerText = element.location;
-
-//           const ticketButton = document.createElement("button");
-//           ticketButton.classList.add("show-table__button");
-//           ticketButton.innerText = "BUY TICKETS";
-
-//           tableRow.appendChild(showDate);
-//           tableRow.appendChild(showVenue);
-//           tableRow.appendChild(showLocation);
-//           tableRow.appendChild(ticketButton);
-//           showTableBody.appendChild(tableRow);
-//         });
-//       };
-//     })
-//     .catch((err) => {
-//       console.log(error);
-//     });
-// };
-
-// getComments();
 
 // Shows table Construction:
 
@@ -129,7 +50,6 @@ const constructShowTable = () => {
   axios
     .get(showsAPI)
     .then((result) => {
-      console.log(result);
       const commentContent = result.data.forEach((element) => {
         const tableRow = document.createElement("tr");
         tableRow.classList.add("show-table__show-row");
@@ -162,7 +82,7 @@ const constructShowTable = () => {
     });
 };
 
-constructShowTable(showsAPI);
+constructShowTable();
 
 // active row event listener
 
@@ -173,107 +93,3 @@ activeRow.forEach((activeRow) => {
     activeRow.classList.add("show-table__show-row:active");
   });
 });
-
-// axios
-//   .get(showsAPI)
-//   .then((result) => {
-//     console.log(result);
-//     const commentContent = (element) => {
-//       console.log(commentContent);
-//       for (let i = 0; i < showsArray.length; i++) {
-//         const tableRow = document.createElement("tr");
-//         tableRow.classList.add("show-table__show-row");
-
-//         const showDate = document.createElement("td");
-//         showDate.classList.add("show-table__text--date");
-//         showDate.innerText = element.date;
-
-//         const showVenue = document.createElement("td");
-//         showVenue.classList.add("show-table__text");
-//         showVenue.innerText = element.place;
-
-//         const showLocation = document.createElement("td");
-//         showLocation.classList.add("show-table__text");
-//         showLocation.innerText = element.location;
-
-//         const ticketButton = document.createElement("button");
-//         ticketButton.classList.add("show-table__button");
-//         ticketButton.innerText = "BUY TICKETS";
-
-//         showTableBody.appendChild(tableRow);
-//         tableRow.appendChild(showDate);
-//         tableRow.appendChild(showVenue);
-//         tableRow.appendChild(showLocation);
-//         tableRow.appendChild(ticketButton);
-//       }
-//     };
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// for (let i = 0; i < showsArray.length; i++) {
-//   const tableRow = document.createElement("tr");
-//   tableRow.classList.add("show-table__show-row");
-
-//   const showDate = document.createElement("td");
-//   showDate.classList.add("show-table__text--date");
-//   showDate.innerText = showsArray[i].date;
-
-//   const showVenue = document.createElement("td");
-//   showVenue.classList.add("show-table__text");
-//   showVenue.innerText = showsArray[i].place;
-
-//   const showLocation = document.createElement("td");
-//   showLocation.classList.add("show-table__text");
-//   showLocation.innerText = showsArray[i].location;
-
-//   const ticketButton = document.createElement("button");
-//   ticketButton.classList.add("show-table__button");
-//   ticketButton.innerText = "BUY TICKETS";
-
-//   showTableBody.appendChild(tableRow);
-//   tableRow.appendChild(showDate);
-//   tableRow.appendChild(showVenue);
-//   tableRow.appendChild(showLocation);
-//   tableRow.appendChild(ticketButton);
-// }
-
-// axios
-//   .get(showsAPI)
-//   .then((result) => {
-//     console.log(result);
-//     const commentContent = (element) => {
-//       console.log(commentContent);
-//       element.forEach(() => {
-//         const tableRow = document.createElement("tr");
-//         tableRow.classList.add("show-table__show-row");
-
-//         const showDate = document.createElement("td");
-//         showDate.classList.add("show-table__text--date");
-//         showDate.innerText = element.date;
-
-//         const showVenue = document.createElement("td");
-//         showVenue.classList.add("show-table__text");
-//         showVenue.innerText = element.place;
-
-//         const showLocation = document.createElement("td");
-//         showLocation.classList.add("show-table__text");
-//         showLocation.innerText = element.location;
-
-//         const ticketButton = document.createElement("button");
-//         ticketButton.classList.add("show-table__button");
-//         ticketButton.innerText = "BUY TICKETS";
-
-//         tableRow.appendChild(showDate);
-//         tableRow.appendChild(showVenue);
-//         tableRow.appendChild(showLocation);
-//         tableRow.appendChild(ticketButton);
-//         showTableBody.appendChild(tableRow);
-//       }
-//       );
-//     };
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
